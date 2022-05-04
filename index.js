9a1a49cce9dd0b7ea9f4ca6b19ac33f67886b49f
OnToologyUser <ontoology@delicias.dia.fi.upm.es>
2022-05-04 13:46:17 +0200

    ontoology generated

--- index.js
+++
@@ -1,8 +0,0 @@
-const N3 = require('n3');
-const fs = require('fs')
-
-const parser = new N3.Parser();
-
-const rdfStream = fs.createReadStream('./isa-glomicave.ttl');
-
-parser.parse(rdfStream, console.log);
